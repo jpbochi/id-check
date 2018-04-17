513546557b458e83ce810c51366a6d8afe158fc1
JP Bochi <jpbochi@protonmail.ch>
2018-04-17 16:14:21 +0200

    adds first version of sign and verify scripts

---
+++ index.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const _ = require('lodash');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const sshpk = require('sshpk');
+const crypto = require('crypto');
+const VError = require('verror');
+
+// const readPrivateKey = () => (fs.readFileSync('./id_rsa.secret').toString());
+// const sign = (message, privateKey) => {
+//   const sign = crypto.createSign('SHA256');
+//   sign.update(message);
+//   return sign.sign(privateKey, 'hex')
+// };
+
+const verify = (message, publicKey, signature) => {
+  const verify = crypto.createVerify('SHA256');
+  verify.update(message);
+  return verify.verify(publicKey, signature, 'base64');
+};
+
+const fetchNotOkError = (res, body) => (
+  new VError({
+    name: 'FetchNotOkError',
+    info: {
+      url: res.url,
+      status: res.status,
+      body
+    }
+  }, `Received ${res.status} response when calling customer API.`)
+);
+
+const handleResponseIfNotOk = (res) => {
+  if (!res.ok) {
+    return res.text().then(body => Promise.reject(fetchNotOkError(res, body)));
+  }
+  return res;
+};
+
+const getGitHubKeys = (user) => (
+  fetch(`https://api.github.com/users/${user}/keys`)
+    .then(handleResponseIfNotOk)
+    .then(_.method('json'))
+    .then(ghKeys => _.map(ghKeys, _.property('key')))
+    .then(sshKeys => (
+      _.map(sshKeys, sshKey => sshpk.parseKey(sshKey).toString('pkcs1'))
+    ))
+);
+
+const messagePayload = _.get(process.argv, 2);
+const signature = _.trim(_.get(process.argv, 3));
+
+const message = JSON.parse(messagePayload);
+const { user, time } = message;
+
+console.log(`> getting GitHub keys for ${user}...`);
+return getGitHubKeys(user)
+  .then(keys => {
+    console.log('> verifying message signature against GitHub keys...');
+    const goodKey = _.find(keys, key => verify(messagePayload, key, signature));
+    if (goodKey) {
+      console.log(`> signature looks good. I trust you are ${user}.`)
+      console.info('>>>', { message, signature }, '<<<');
+    } else {
+      return Promise.reject(new VError({ name: 'BadSignatureError', info: { user, keys }}));
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
